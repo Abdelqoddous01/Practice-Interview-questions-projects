1c957e0bba059b30167e8505d68bfe7728ef0768
Abdelqoddous01 <144550050+Abdelqoddous01@users.noreply.github.com>
2023-10-17 22:33:36 +0100

    Add files via upload

---
+++ binary-represention.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+int main() {
+    int n;
+    int k;
+    printf("Enter a number :");
+    scanf("%d", &n);
+    unsigned arr1[31] = {0}; 
+    char choice[100];
+    //fgets(choice,100,stdin);
+
+    int len1 = sizeof(arr1) / sizeof(arr1[0]);
+    printf("Binary forme :");
+    for (int i = len1 - 1; i >= 0; i--) {
+        arr1[i] = n % 2;
+        n = n / 2;}
+    for (int i = 0; i < len1; i++)
+    {
+        printf("%d",arr1[i]);
+    }
+}
