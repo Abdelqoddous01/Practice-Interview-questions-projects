ddbdc7beef50bc4c6a402cdc5e1f973ba191f85b
Abdelqoddous01 <144550050+Abdelqoddous01@users.noreply.github.com>
2023-10-18 17:03:17 +0100

    Add files via upload

---
+++ from_binary_to_digit.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+#include <string.h>
+#include <stdlib.h>
+
+int s(int n,int a,int b, int c){
+    while (n>3)
+    {
+       return n+(n-1);
+    }
+}
+
+int main() {
+    printf("%d",s(5,1,2,3));
+    return 0;
+}
