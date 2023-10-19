5726767e85d642e7fd8400471d444afdebea7818
Abdelqoddous01 <144550050+Abdelqoddous01@users.noreply.github.com>
2023-10-19 12:18:27 +0100

    Add files via upload

---
+++ upper_case_strings.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+#include <string.h>
+#include <ctype.h>
+
+int main(){
+    char s[100];
+    char up[100];
+    fgets(s,100,stdin);
+    int len =strlen(s);
+    for (int i = 0; i < len; i++)
+    {
+        up[i]= toupper (s[i]);
+    }
+    for ( int i = 0; i < len; i++)
+    {
+        printf("%c",up[i]);
+    }
+    
+}
