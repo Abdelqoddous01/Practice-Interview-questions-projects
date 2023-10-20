475c4ba25de2336a8676d929458db0861edb8a59
Abdelqoddous01 <144550050+Abdelqoddous01@users.noreply.github.com>
2023-10-20 16:00:59 +0100

    Add files via upload

---
+++ encryption_and_decryption_message.c
@@ -0,0 +1,33 @@
+#include <stdio.h>
+#include <string.h>
+
+
+int main(void){
+    char s[100];
+    fgets(s,100,stdin);
+    for (int i = 0; i < strlen(s); i++)
+    {
+        if (s[i]>='a' && s[i]<='z')
+        {
+            s[i]='a'+(s[i]-'a'+3)%26;
+        }
+        else if(s[i]>='A' && s[i]<='Z'){
+            s[i]='A'+(s[i]-'A'+3)%26;
+        }
+    }
+        
+    printf("The encrypted message : %s",s);
+        for (int i = 0; i < strlen(s); i++)
+    {
+        if (s[i]>='a' && s[i]<='z')
+        {
+            s[i]='a'+(s[i]-'a'-3);
+        }
+        else if(s[i]>='A' && s[i]<='Z'){
+            s[i]='A'+(s[i]-'A'-3);
+        }
+    }
+    
+     printf("The original message : %s",s);
+    return 0;
+}
